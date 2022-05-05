a5c965e85b0992cf3f5a8329e4b4cc6713c6025a
dding <44263736+Han1015@users.noreply.github.com>
2022-05-06 09:24:22 +1000

    delete counter

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
 import postReducer from "../Containers/Webinars/webinarsSlice";
 import loginReducer from "../features/login/loginSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     posts: postReducer,
     login: loginReducer,
   },
